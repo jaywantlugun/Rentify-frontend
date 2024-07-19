a3eb89f025ce8840a831ea4575b6e86f7f497127
Jaywant Lugun <jaywantlugun2000@gmail.com>
2024-07-20 01:16:26 +0530

    updated to render url

--- src/api/ApiClient.js
+++ src/api/ApiClient.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const apiClient = axios.create(
     {
-        baseURL: 'https://rentify-backend-production.up.railway.app/' 
+        //baseURL: 'https://rentify-backend-production.up.railway.app/' 
+        baseURL: 'https://rentify-backend-sesy.onrender.com'
     }
 );
